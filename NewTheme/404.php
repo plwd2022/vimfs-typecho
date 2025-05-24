d937158e4448aadfd663569292e24b4d729fb025
plwd <admin@plwd.xyz>
2025-05-24 12:33:08 +0800

    '清空此库开始下一步开发'

--- NewTheme/404.php
+++
@@ -1,9 +0,0 @@
-<?php $this->need('header.php'); ?>
-<div id="main">
-  <div class="post">
-    <h2 class="post-title" id="autord" role="tab" tabindex="0" accesskey="x">404 - 页面未找到</h2>
-    <div class="post-content"><table><tr><td><p>抱歉，您要查看的页面已被转移或不存在！也许可以试试搜索？</p></td></tr></table></div>
-  </div>
-</div>
-<?php $this->need('sidebar.php'); ?>
-<?php $this->need('footer.php'); ?>
